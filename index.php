e279624e762878a9df1c4ac2d321db9825b531c5
Klaus Heino <klaus.heino@gmail.com>
2015-06-01 19:32:49 +0300

    vihreaa valoa

--- index.php
+++ index.php
@@ -1,10 +1,13 @@
 <!DOCTYPE html>
 <html>
 	<head>
-	
+		<title>Profile site</title>
+		<meta charset="utf-8">
+		<link rel="stylesheet" href="css/styles.css"/>
 	</head>
 	
 	<body>
+		<h1>Otsikko</h1>
 		<p>lollolol</p>
 	</body>
 
