98117684fb41ef2105e25b6b41844bc3d76a323a
Pekka Pekkonen <pekka.pekkonen@live.fi>
2015-06-03 22:22:49 +0300

    Tehty labelit ja tekstikentät.

---
+++ add_post.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<html>
+	<head>
+		<title>Add a new blog post</title>
+		<meta charset="UTF-8">
+		<link rel="stylesheet" href= "css/styles.css">
+	</head>
+	
+	<body>
+		<div class="wrapper">
+		
+			<div class="input-field">
+				<label for="author">Author</label>
+				<input type="text" id="author">
+			</div>
+			
+			<div class="input-field">
+				<label for="date">Date</label>
+				<input type="text" id="date">
+			</div>
+			
+			<div class="input-field">
+				<label for="title">Title</label>
+				<input type="text" id="title">
+			</div>
+			
+			<div class="input-field">
+				<label for="content">Content</label>
+				<textarea rows="30" cols="70" id="content"></textarea>
+			</div>
+			
+		</div>
+	</body>
+	
+</html>
+
+
