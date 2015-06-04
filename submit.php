6ee8b1025149abf71e6ea694d16892541e863273
Pekka Pekkonen <pekka.pekkonen@live.fi>
2015-06-04 23:47:19 +0300

    Tarkastetaan, että käyttäjä on antanut authorin, titlen ja contentin. Mikäli tarkastus ei mene läpi, palataan takaisin tyhjään lomakkeeseen.

--- submit.php
+++ submit.php
@@ -1,14 +1,19 @@
 <?php
 include_once('database.php');
 
-$sql = 'INSERT INTO post(author, title, content) VALUES (:author, :title, :content)';
+if( !empty($_POST['author']) && !empty($_POST['title']) && !empty($_POST['content'])) {
+	$sql = 'INSERT INTO post(author, title, content) VALUES (:author, :title, :content)';
 
-$stmt = $handler->prepare($sql);
+	$stmt = $handler->prepare($sql);
 
-$stmt->bindParam(':author', $_POST['author'], PDO::PARAM_STR);
-$stmt->bindParam(':title', $_POST['title'], PDO::PARAM_STR);
-$stmt->bindParam(':content', $_POST['content'], PDO::PARAM_STR);
+	$stmt->bindParam(':author', $_POST['author'], PDO::PARAM_STR);
+	$stmt->bindParam(':title', $_POST['title'], PDO::PARAM_STR);
+	$stmt->bindParam(':content', $_POST['content'], PDO::PARAM_STR);
 
-$stmt->execute();
-header('Location: index.php');
+	$stmt->execute();
+	header('Location: index.php');
+} else {
+	header('Location: add_post.php');
+}
+
 ?>
