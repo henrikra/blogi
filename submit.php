60588a1e7ab2edc6a83501e5a2827a3808318d27
Pekka Pekkonen <pekka.pekkonen@live.fi>
2015-06-05 02:11:00 +0300

    Estetty html-tagien toiminta.

--- submit.php
+++ submit.php
@@ -1,14 +1,18 @@
 <?php
 include_once('database.php');
 
+function e($string) {
+	return htmlentities($string, ENT_QUOTES, 'UTF-8', false);
+}
+
 if( !empty($_POST['author']) && !empty($_POST['title']) && !empty($_POST['content'])) {
 	$sql = 'INSERT INTO post(author, title, content) VALUES (:author, :title, :content)';
 
 	$stmt = $handler->prepare($sql);
 
-	$stmt->bindParam(':author', $_POST['author'], PDO::PARAM_STR);
-	$stmt->bindParam(':title', $_POST['title'], PDO::PARAM_STR);
-	$stmt->bindParam(':content', $_POST['content'], PDO::PARAM_STR);
+	$stmt->bindParam(':author', e($_POST['author']), PDO::PARAM_STR);
+	$stmt->bindParam(':title', e($_POST['title']), PDO::PARAM_STR);
+	$stmt->bindParam(':content', e($_POST['content']), PDO::PARAM_STR);
 
 	$stmt->execute();
 	header('Location: index.php');
