4d51d211b38b030ca1296f1658e7aea97a62706a
Pekka Pekkonen <pekka.pekkonen@live.fi>
2015-06-05 01:49:04 +0300

    Lisätty bio-kuva ja sen rakenne.

--- index.php
+++ index.php
@@ -21,11 +21,11 @@ $query = $handler->query('SELECT * FROM post ORDER BY postDatetime DESC;');
 			
 			<div class="main-content">
 				<?php while($r = $query->fetch(PDO::FETCH_OBJ)) : ?>
-				<div class="post">
+				<div class="post panel">
 					<div class="post-image">
 						<img src="img/korea-field.jpg" alt="Default picture">
 					</div>
-					<div class="post-container">
+					<div class="panel-container">
 						<h2><?php echo $r->title; ?></h2>
 						<div class="post-info"><?php echo date('D j.n.Y \- H:i', strtotime($r->postDatetime)) . " / " . $r->author; ?></div>
 						<p><?php echo nl2br($r->content); ?></p>
@@ -34,17 +34,19 @@ $query = $handler->query('SELECT * FROM post ORDER BY postDatetime DESC;');
 				<?php endwhile; ?>
 			</div><!-- main-content -->
 			<aside>
-				<div class="bio">
+				<div class="bio panel">
 					<div class="bio-background">
 						<img src="img/bio-background.jpg" alt="Bio background">
 					</div>
-					<img src="img/me.jpg" alt="Erik Rantanen">
-					<div class="bio-name">
-						Erik Rantanen
+					<div class="bio-face-image">
+						<img src="http://s14.postimg.org/jtmz3pd1d/index.jpg" alt="Erik Rantanen">
 					</div>
-					<div class="bio-description">
-						Programmer, son, chili head, revolvist...
-					</div>
+					<div class="panel-container">
+						<h2>Erik Rantanen</h2>
+						<div class="bio-description">
+							Programmer, son, chili head, revolvist...
+						</div>
+					</div><!-- panel-container -->
 				</div>
 			</aside>
 			<div class="clearfix"></div>
