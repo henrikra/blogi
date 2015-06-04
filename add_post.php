7fa078e71eab4a5375094f4928ae146da611e4df
Pekka Pekkonen <pekka.pekkonen@live.fi>
2015-06-04 20:29:16 +0300

    Lisätty name attribuutti kaikille syöttökentille.

--- add_post.php
+++ add_post.php
@@ -15,22 +15,22 @@
 			
 				<div class="form-row">
 					<label class="form-label" for="author">Author</label><!--
-					--><input class="form-input" type="text" id="author">
+					--><input class="form-input" type="text" id="author" name="author">
 				</div>
 								
 				<div class="form-row">
 					<label class="form-label" for="title">Title</label><!--
-					--><input class="form-input" type="text" id="title">
+					--><input class="form-input" type="text" id="title" name="title">
 				</div>
 				
 				<div class="form-row">
 					<label class="form-label" for="content">Content</label><!--
-					--><textarea class="form-input" rows="13" cols="70" id="content"></textarea>
+					--><textarea class="form-input" rows="13" cols="70" id="content" name="content"></textarea>
 				</div>
 				
 				<div class="form-row">
 					<label class="form-label" for="tags">Tags</label><!--
-					--><select class="form-input" id="tags">
+					--><select class="form-input" id="tags" name="content">
 						<option value="ruoka">Ruoka</option>
 						<option value="kulttuuri">Kulttuuri</option>
 						<option value="ohjelmointi">Ohjelmointi</option>
