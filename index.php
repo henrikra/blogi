b15f3985e3d3048eafb3b85aa9c416caf8835c01
Pekka Pekkonen <pekka.pekkonen@live.fi>
2015-06-04 22:14:48 +0300

    Lisätty charset=utf-8

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 	</head>
 <?php 
 try {
-	$handler = new PDO('mysql:host=127.0.0.1;dbname=blogi', 'root', '');
+	$handler = new PDO('mysql:host=127.0.0.1;dbname=blogi;charset=utf8', 'root', '');
 	$handler->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch(PDOException $e) {
 	echo $e->getMessage();
