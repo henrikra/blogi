0f22f4ffadb9499c244622faa970ae1a7d77b313
Pekka Pekkonen <pekka.pekkonen@live.fi>
2015-06-06 01:59:12 +0300

    Näytetään sivupaneeli includauksen avulla.

--- index.php
+++ index.php
@@ -67,22 +67,7 @@ $query = $handler->query('SELECT * FROM post ORDER BY postDatetime DESC;');
 				</div><!-- post -->
 				<?php endwhile; ?>
 			</div><!-- main-content -->
-			<aside>
-				<div class="bio panel">
-					<div class="bio-background">
-						<img class="vertical-center" src="img/bio-background.jpg" alt="Bio background">
-					</div>
-					<div class="bio-face-image">
-						<img class="vertical-center" src="img/Erik.jpg" alt="Erik Rantanen">
-					</div>
-					<div class="panel-container">
-						<h2>Erik Rantanen</h2>
-						<div class="bio-description">
-							Programmer, son, chili head, revolvist...
-						</div>
-					</div><!-- panel-container -->
-				</div>
-			</aside>
+			<?php include_once('sidebar.php'); ?>
 		</div><!-- wrapper -->
 	</body>
 
