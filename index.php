f1996c9b276fd80fe8db2bf8b321e83c4da542fd
Henrik Raitasola <heka.fof@gmail.com>
2015-06-05 20:54:51 +0300

    Clearfix luokka laitetaan nyt flouttaavien elementtien parentille. Pystysuuntainen keskitys refaktoroitu omaan luokkaan, koska se on yleiskäyttöinen.

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ $query = $handler->query('SELECT * FROM post ORDER BY postDatetime DESC;');
 
 ?>
 	<body>
-		<div class="wrapper">
+		<div class="wrapper clearfix">
 			<header>
 				<h1>Erik's Blogi</h1>
 				<a href="add_post.php">Add post</a>
@@ -23,7 +23,8 @@ $query = $handler->query('SELECT * FROM post ORDER BY postDatetime DESC;');
 				<?php while($r = $query->fetch(PDO::FETCH_OBJ)) : ?>
 				<div class="post panel">
 					<div class="post-image">
-						<img src="<?php echo (!empty($r->imageLocation) ? $r->imageLocation : 'uploads/korea-field.jpg');?>"> <!-- If-lauseen short hand syntax -->
+						<img class="vertical-center"
+						src="<?php echo (!empty($r->imageLocation) ? $r->imageLocation : 'uploads/korea-field.jpg');?>"> <!-- If-lauseen short hand syntax -->
 					</div>
 					<div class="panel-container">
 						<h2><?php echo $r->title; ?></h2>
@@ -39,7 +40,7 @@ $query = $handler->query('SELECT * FROM post ORDER BY postDatetime DESC;');
 						<img src="img/bio-background.jpg" alt="Bio background">
 					</div>
 					<div class="bio-face-image">
-						<img src="http://s14.postimg.org/jtmz3pd1d/index.jpg" alt="Erik Rantanen">
+						<img class="vertical-center" src="http://s14.postimg.org/jtmz3pd1d/index.jpg" alt="Erik Rantanen">
 					</div>
 					<div class="panel-container">
 						<h2>Erik Rantanen</h2>
@@ -49,7 +50,6 @@ $query = $handler->query('SELECT * FROM post ORDER BY postDatetime DESC;');
 					</div><!-- panel-container -->
 				</div>
 			</aside>
-			<div class="clearfix"></div>
 		</div><!-- wrapper -->
 	</body>
 
