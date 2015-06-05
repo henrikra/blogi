4640d3536bd2cafcdd195d58f6e967aef4c1c7f8
Pekka Pekkonen <pekka.pekkonen@live.fi>
2015-06-06 00:30:07 +0300

    Kirjoitusvirhe korjattu.

--- helpers.php
+++ helpers.php
@@ -6,7 +6,7 @@ function e($string) {
 
 function getExcerpt($str, $startPos=0, $maxLength=100) {
 	if(strlen($str) > $maxLength) {
-		$exerpt = substr($str, $startPos, $maxLength-3);
+		$excerpt = substr($str, $startPos, $maxLength-3);
 		$lastSpace = strrpos($excerpt, ' ');
 		$excerpt = substr($excerpt, 0, $lastSpace);
 		$excerpt .= '...';
