4fb6a510987abd838450adca832564c89f8420bf
Pekka Pekkonen <pekka.pekkonen@live.fi>
2015-06-05 19:22:03 +0300

    Lisää tietokantaan myös kuvan. Kuvaa ei validoida mitenkään.

--- submit.php
+++ submit.php
@@ -6,17 +6,33 @@ function e($string) {
 }
 
 if( !empty($_POST['author']) && !empty($_POST['title']) && !empty($_POST['content'])) {
-	$sql = 'INSERT INTO post(author, title, content) VALUES (:author, :title, :content)';
+	$sql = 'INSERT INTO post(author, title, content, imageLocation) VALUES (:author, :title, :content, :imageLocation)';
 
 	$stmt = $handler->prepare($sql);
 
 	$stmt->bindParam(':author', e($_POST['author']), PDO::PARAM_STR);
 	$stmt->bindParam(':title', e($_POST['title']), PDO::PARAM_STR);
 	$stmt->bindParam(':content', e($_POST['content']), PDO::PARAM_STR);
+	
+	/* Preparing picture */
+	
+	$imageName = $_FILES['image']['name'];
+	echo $imageName;
+	
+	$tmp_location = $_FILES['image']['tmp_name'];
+	$wanted_location = 'img\\uploads\\' . $imageName;
+	
+	move_uploaded_file($tmp_location, $wanted_location);
+	
+	$stmt->bindParam(':imageLocation', $wanted_location, PDO::PARAM_STR);
 
+	/* Executing SQL query */
 	$stmt->execute();
+	
+	/* Redirecting to main page*/
 	header('Location: index.php');
 } else {
+	/* Redirecting back to blank form */
 	header('Location: add_post.php');
 }
 
