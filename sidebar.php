b70894e2ec60aa57fc816f2a362f6177b2ba2b1a
Pekka Pekkonen <pekka.pekkonen@live.fi>
2015-06-06 02:02:00 +0300

    Tämä sidebar on irroitettu index.php:stä. Yleiskäyttöinen.

---
+++ sidebar.php
@@ -0,0 +1,16 @@
+<aside>
+	<div class="bio panel">
+		<div class="bio-background">
+			<img class="vertical-center" src="img/bio-background.jpg" alt="Bio background">
+		</div>
+		<div class="bio-face-image">
+			<img class="vertical-center" src="img/Erik.jpg" alt="Erik Rantanen">
+		</div>
+		<div class="panel-container">
+			<h2>Erik Rantanen</h2>
+			<div class="bio-description">
+				Programmer, son, chili head, revolvist...
+			</div>
+		</div><!-- panel-container -->
+	</div>
+</aside>
