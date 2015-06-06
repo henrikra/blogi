286ac3ab5962e1eb15dbe78315b6f9e818883e38
Pekka Pekkonen <pekka.pekkonen@live.fi>
2015-06-06 18:38:35 +0300

    Dynaaminen korostus, joka aktivoituu kun ollaan kyseisellä sivulla.

--- header.php
+++ header.php
@@ -6,7 +6,9 @@
 					<a class="vertical-center" href="index.php">Main logo</a>
 				</li>
 				<li class="menu-item"><a class="vertical-center" href="">About me</a></li>
-				<li class="menu-item"><a class="vertical-center" href="add_post.php">Add post</a></li>
+				<li class="menu-item <?php echo basename($_SERVER['SCRIPT_NAME']) == 'add_post.php' ? 'active' : ''; ?>">
+					<a class="vertical-center" href="add_post.php">Add post</a>
+				</li>
 				<li class="menu-item <?php echo basename($_SERVER['SCRIPT_NAME']) == 'index.php' ? 'active' : ''; ?>">
 					<a class="vertical-center" href="index.php">Home</a>
 				</li>
