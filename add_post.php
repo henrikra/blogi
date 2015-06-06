0465f983cb1a131dafe22c1cb37214d5fb794cf4
Pekka Pekkonen <pekka.pekkonen@live.fi>
2015-06-06 18:39:18 +0300

    Lisätty luokkia, jotka tekevät sivusta yhdennäköisemmän muiden sivujen kanssa.

--- add_post.php
+++ add_post.php
@@ -9,61 +9,65 @@
 	<?php include_once('database.php'); ?>
 
 	<body>
-		<div class="wrapper">
-		
-			<h1>Add a new blog post</h1>
-		
-			<form action="submit.php" method="post" enctype="multipart/form-data">
-			
-				<div class="form-row">
-					<label class="col-2" for="author">Author</label>
-					<div class="col-8">
-						<input type="text" id="author" name="author">
-					</div>
-				</div>
-								
-				<div class="form-row">
-					<label class="col-2" for="title">Title</label>
-					<div class="col-8">
-						<input type="text" id="title" name="title">
-					</div>
-				</div>
+		<?php include_once('header.php'); ?>
+		<div class="wrapper clearfix">
+			<div class="main-content panel">
+				<div class="panel-container">
+					<h1>Add a new blog post</h1>
 				
-				<div class="form-row">
-					<label class="col-2" for="content">Content</label>
-					<div class="col-8">
-						<textarea rows="13" id="content" name="content"></textarea>
-					</div>
-				</div>
-				
-				<div class="form-row">
-					<label class="col-2" for="file">Picture</label>
-					<div class="col-8">
-						<input type="file" id="file" name="image">
-					</div>
-				</div>
-				
-				<div class="form-row">
-					<label class="col-2">Tags</label>
-					<div class="col-8">						
-						<?php
-							$query = $handler->query('SELECT * FROM tag');
-							while($r = $query->fetch(PDO::FETCH_OBJ)) : ?>
-							<label>
-								<input class="cb" type="checkbox" name="tags[]" value="<?php echo $r->tagId; ?>"> <?php echo $r->tagName;?>
-							</label>
-							<?php endwhile; ?>
-					</div>
-				</div>
-				
-				<div class="form-row">
-					<label class="col-2"></label>
-					<div class="col-8">
-						<input type="submit" value="Submit">
-					</div>
-				</div>	
-			</form>
-			
+					<form action="submit.php" method="post" enctype="multipart/form-data">
+					
+						<div class="form-row">
+							<label class="col-2" for="author">Author</label>
+							<div class="col-8">
+								<input type="text" id="author" name="author">
+							</div>
+						</div>
+										
+						<div class="form-row">
+							<label class="col-2" for="title">Title</label>
+							<div class="col-8">
+								<input type="text" id="title" name="title">
+							</div>
+						</div>
+						
+						<div class="form-row">
+							<label class="col-2" for="content">Content</label>
+							<div class="col-8">
+								<textarea rows="13" id="content" name="content"></textarea>
+							</div>
+						</div>
+						
+						<div class="form-row">
+							<label class="col-2" for="file">Picture</label>
+							<div class="col-8">
+								<input type="file" id="file" name="image">
+							</div>
+						</div>
+						
+						<div class="form-row">
+							<label class="col-2">Tags</label>
+							<div class="col-8">						
+								<?php
+									$query = $handler->query('SELECT * FROM tag');
+									while($r = $query->fetch(PDO::FETCH_OBJ)) : ?>
+									<label>
+										<input class="cb" type="checkbox" name="tags[]" value="<?php echo $r->tagId; ?>"> <?php echo $r->tagName;?>
+									</label>
+									<?php endwhile; ?>
+							</div>
+						</div>
+						
+						<div class="form-row">
+							<label class="col-2"></label>
+							<div class="col-8">
+								<input type="submit" value="Submit">
+							</div>
+						</div>	
+					</form>
+				</div><!-- panel-container -->
+			</div> <!-- main content -->
+			<?php include_once('sidebar.php'); ?>
 		</div>
 	</body>
 	
