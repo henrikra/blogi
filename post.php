1b79961795b7e03eae96b464f4cd9d1ce1e8af18
Henrik Raitasola <heka.fof@gmail.com>
2015-06-07 17:31:43 +0300

    Tagien generointi eristetty omaan funktioon

--- post.php
+++ post.php
@@ -34,23 +34,7 @@
 							<i class="fa fa-calendar"></i>
 								<?php echo date('D j.n.Y \- H:i', strtotime($post->postDatetime)) . ' / ';?>
 								<i class="fa fa-user"></i> <?php echo $post->author; ?>
-								<?php 
-								$sql = "SELECT tag.tagName FROM tag INNER JOIN posttag ON tag.tagId = posttag.tagId WHERE posttag.postID = :postId;";
-								$stmt = $handler->prepare($sql);
-								
-								$stmt->bindParam(':postId', $postId, PDO::PARAM_INT);
-								$stmt->execute();
-								
-								$tags = ' / <i class="fa fa-tags"></i> ';
-								$counter = 0;
-								while($post2 = $stmt->fetch(PDO::FETCH_OBJ)) :
-									$tags .= $post2->tagName . ', ';
-									$counter++;
-								endwhile;
-								if ($counter > 0) {
-									echo substr($tags, 0, -2);
-								}
-								?>
+								<?php getTags($postId);	?>
 						</div>
 						<?php if(!empty($post->imageLocation)) : ?>
 							<?php $picture = $post->imageLocation;?>
