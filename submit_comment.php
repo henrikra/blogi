be5629d38d135d4f6fcc2a12affdcfe9415729a9
Pekka Pekkonen <pekka.pekkonen@live.fi>
2015-06-17 23:44:03 +0300

    Kommentoinnin cool down aikaan liittyvää refaktorointia. Kommenttien nimiä muutettu ja joitain poistettu.

--- submit_comment.php
+++ submit_comment.php
@@ -8,24 +8,23 @@ $postId = e($_POST['postId']);
 
 /*--- Spämmisesto ---*/
 // Tarkastetaan, onko session aikana tehty aikaisempi kommentti
-$lastCommentExists = isset($_SESSION['lastOwnCommentTime']);
+$ableToComment = true;
 // Jos on tehty aikaisempi kommentti, tutkitaan onko kulunut riittävä aika siitä
-if ($lastCommentExists) {
-	$lastOwnCommentTime = $_SESSION['lastOwnCommentTime'];
+if (isset($_SESSION['commentPostTime'])) {
+	$commentPostTime = $_SESSION['commentPostTime'];
 	$currentTime = time();
-	$spamFilterTime = 30;
-	$commentCooledDown = ($currentTime - $lastOwnCommentTime) > $spamFilterTime;
+	$cooldownTime = 30;
+	$ableToComment = $currentTime - $commentPostTime > $cooldownTime;
 }
-$notSpam = (!$lastCommentExists || $commentCooledDown);
 
 /*
-$_SESSION['errors'][] = '!$lastCommentExists: ' . !$lastCommentExists;
+$_SESSION['errors'][] = '!$hasCommented: ' . !$hasCommented;
 $_SESSION['errors'][] = '$currentTime: ' . $currentTime;
-$_SESSION['errors'][] = '$lastOwnCommentTime: ' . $lastOwnCommentTime;
-$_SESSION['errors'][] = 'Spämmiesto :' . (!$lastCommentExists || $commentCooledDown);
+$_SESSION['errors'][] = '$commentPostTime: ' . $commentPostTime;
+$_SESSION['errors'][] = 'Spämmiesto :' . (!$hasCommented || $ableToComment);
 */
 
-if( !empty($_POST['postId']) && !empty($_POST['commentAuthor']) && !empty($_POST['commentContent']) && $notSpam){
+if( !empty($_POST['postId']) && !empty($_POST['commentAuthor']) && !empty($_POST['commentContent']) && $ableToComment){
 		
 	$sql = "INSERT INTO postcomment (postId, commentAuthor, commentContent, commentReply) VALUES (:postId, :commentAuthor, :commentContent, :commentReply);";
 
@@ -48,17 +47,17 @@ if( !empty($_POST['postId']) && !empty($_POST['commentAuthor']) && !empty($_POST
 	
 	$stmt->execute();
 	
-	// Save post's timestamp to session to prevent spam
-	$_SESSION['lastOwnCommentTime'] = time();
+	// Save comment's timestamp to session to prevent ableToComment
+	$_SESSION['commentPostTime'] = time();
 	
 	header('Location: post.php?postId=' . $postId . '#comments');
 	
 } else {
 	
 	/*--- Deciding error message --*/
-	// option 1: comments is spam
-	if(!$notSpam){
-		$_SESSION['errors'][] = 'Comment not submitted. Please try again later.';
+	// option 1: comments is ableToComment
+	if(!$ableToComment){
+		$_SESSION['errors'][] = 'Please post again after a short while.';
 	// option 2: comment has not needed fields
 	} else {
 		/* Store error if name or comment is missing */
