e3fbb3ef5a119e0360bcd4e11180af68a1858817
Pekka Pekkonen <pekka.pekkonen@live.fi>
2015-06-17 17:01:25 +0300

    Lisätty jokaiseen itsenäiseen front-puolen sivuun fonttien tyylilinkki.

--- add_post.php
+++ add_post.php
@@ -5,6 +5,7 @@
 		<meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+		<link href='http://fonts.googleapis.com/css?family=Dosis|Open+Sans' rel='stylesheet' type='text/css'>
 		<link rel="stylesheet" href= "css/styles.css">
 	</head>
 	
--- index.php
+++ index.php
@@ -5,8 +5,8 @@
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
-		<link rel="stylesheet" href="css/styles.css"/>
 		<link href='http://fonts.googleapis.com/css?family=Dosis|Open+Sans' rel='stylesheet' type='text/css'>
+		<link rel="stylesheet" href="css/styles.css"/>
 	</head>
 <?php 
 session_start();
